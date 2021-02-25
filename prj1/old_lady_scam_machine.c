b7927f58b4e23f02ffd1c52ac3cbe4ff8350284f
Charles Bartoletti <cbartol1@cs.binghamton.edu>
2021-02-25 14:04:42 -0500

    second commit

--- prj1/old_lady_scam_machine.c
+++ prj1/old_lady_scam_machine.c
@@ -1,8 +1,10 @@
-int ComputeMaxSteal(int* cards, int cash, int subsets){
+int ComputeMaxSteal(int* cards, int cash, int subsets, int bits){
         int maxSteal = 0;
-        int items;
-        int optimal;
-        int totCost = 0
+        int* items;
+        int* optimal;
+        int totCost = 0;
+	int newCost = 0;
+	int sntl = 0;
         numCards = sizeof(cards)/sizeof(cards[0]);
         for(int i = 0; i < numCards; i++){
                 totCost = totCost + cards[i];
@@ -10,6 +12,27 @@ int ComputeMaxSteal(int* cards, int cash, int subsets){
         if(totCost <= cash){
                 return totCost;
         }
+	else totCost = 0;
         for(int i = 0; i < subsets; i++){
-
+		for(int j = 0; j < bits; j++){
+			if((i >> j) & 1){
+				items[sntl] = cards[j];
+				sntl++;
+			}
+		}
+		for(int r = 0; r < sizeof(items)/sizeof(items[0]); r++){
+			newCost = newCost + items[r];
+		}
+		if(newCost < cash){
+			if(newCost > maxSteal){
+				maxSteal = newCost;
+				newCost = 0;
+				for(int r = 0; r < sizeof(items)/sizeof(items[0]); r++){
+                        		optimal[r] = items[r];
+					items[r] = 0;
+                		}
+			}
+		}
+	}
+	return maxProfit;
 }
