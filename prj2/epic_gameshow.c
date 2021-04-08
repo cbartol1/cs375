2aadda279bf219d8e1adf5faed539331e88b3a2a
Charles Bartoletti <cbartol1@cs.binghamton.edu>
2021-04-07 21:49:17 -0400

    completed project 2

--- prj2/epic_gameshow.c
+++ prj2/epic_gameshow.c
@@ -69,6 +69,7 @@ void buildHeap(heap *list, int *arr, int size){
 }
 
 int flag = -1;
+int sntl = -1;
 
 void searchHeap(heap *list, int i, int id){
         if(LCHILD(i) < list->size){
@@ -85,37 +86,54 @@ void searchHeap(heap *list, int i, int id){
 }
 
 void
-findContestant(int id, heap *list){
+findContestant(int id, heap *list, FILE* fp){
 	searchHeap(list, 0, id);
 	if(flag != -1){
-		printf("Contestant %d is in the extended heap with score %d\n", id+1, list->c[id].points);
+		fprintf(fp, "Contestant <%d> is in the extended heap with score <%d>\n", id+1, list->c[id].points);
 	}
 	else{
-		printf("Contestant %d is not in the extended heap\n", id+1);
+		fprintf(fp, "Contestant <%d> is not in the extended heap\n", id+1);
 	}
 	flag = -1;
 }
 
 void
-insertContestant(int id, int score, heap* list, int *arr){
-	if(list->size){
-		list->c = realloc(list->c, ((list->size)+1) * sizeof(contestant));
-	}
-	else{
-		list->c = malloc(sizeof(contestant));
-	}
+insertContestant(int id, int score, heap* list, int *arr, FILE* fp){
+	//searchHeap(list, 0, id);
+	//FILE *fp = fopen("output.txt", "a");
+	//if(flag != -1){
+		//fprintf(fp, "Contestant <%d> is already in the extended heap: cannot insert.", id);
+	//}
+	//else{
+		if(list->size){
+			searchHeap(list, 0, id);
+			if(flag != -1){
+                		fprintf(fp, "Contestant <%d> is already in the extended heap: cannot insert.\n", id+1);
+        		}
+			else{
+				list->c = realloc(list->c, ((list->size)+1) * sizeof(contestant));
+			}
+		}
+		else{
+			list->c = malloc(sizeof(contestant));
+		}
+		if(flag == -1){
+			contestant c;
+			c.id = id;
+			c.points = score;
 
-	contestant c;
-	c.id = id;
-	c.points = score;
-
-	int i = (list->size)++;
-	while(i && c.points < list->c[PARENT(i)].points){
-		list->c[i] = list->c[PARENT(i)];
-		i = PARENT(i);
-	}
-	arr[id] = score;
-	list->c[i] = c;
+			int i = (list->size)++;
+			while(i && c.points < list->c[PARENT(i)].points){
+				list->c[i] = list->c[PARENT(i)];
+				i = PARENT(i);
+			}
+			arr[id] = score;
+			list->c[i] = c;
+			fprintf(fp, "Contestant <%d> inserted with initial score <%d>\n", id+1, score);
+		}
+	//}
+	//fclose(fp);
+	flag = -1;
 }
 
 //void swap(contestant *c1, contestant *c2){
@@ -139,9 +157,11 @@ insertContestant(int id, int score, heap* list, int *arr){
 //}
 
 void
-eliminateWeakest(heap* list, int *arr){
+eliminateWeakest(heap* list, int *arr, FILE* fp){
 	if(list->size){
-		printf("Contestant %d with current lowest score %d eliminated\n", list->c[0].id+1, list->c[0].points);
+		if(sntl == -1){
+			fprintf(fp, "Contestant <%d> with current lowest score %d eliminated\n", list->c[0].id+1, list->c[0].points);
+		}
 		arr[list->c[0].id] = 0;
 		list->c[0] = list->c[--(list->size)];
 		//arr[list->c[0].id] = 0;
@@ -149,7 +169,7 @@ eliminateWeakest(heap* list, int *arr){
 		heapify(list, 0);
 	}
 	else{
-		printf("no players in da game yet\n");
+		fprintf(fp, "No contestant can be eliminated since the extended heap is empty\n");
 		free(list->c);
 	}
 }
@@ -171,43 +191,46 @@ eliminateWeakest(heap* list, int *arr){
 //}
 
 void
-earnPoints(int id, int points, heap* list, int *arr){
+earnPoints(int id, int points, heap* list, int *arr, FILE* fp){
 	//int flag = -1;
 	searchHeap(list, 0, id);
 	if(flag == -1){
-		printf("Contestant %d is not in the heap\n", id);
+		fprintf(fp, "Contestant <%d> is not in the extended heap\n", id+1);
 	}
 	else{
 		list->c[flag].points = list->c[flag].points + points;
 		arr[list->c[flag].id] = list->c[flag].points;
+		fprintf(fp, "Contestant <%d>s score increased by <%d> points to <%d>\n", id+1, points, list->c[flag].points);
 		heapify(list, 0);
 	}
 	flag = -1;
 }
 
 void
-lostPoints(int id, int points, heap* list, int *arr){
+lostPoints(int id, int points, heap* list, int *arr, FILE* fp){
 	//int flag = -1;
 	searchHeap(list, 0, id);
         if(flag == -1){
-                printf("Contestant %d is not in the heap\n", id);
+                fprintf(fp, "Contestant <%d> is not in the extended heap\n", id+1);
         }
         else{
                 list->c[flag].points = list->c[flag].points - points;
 		arr[list->c[flag].id] = list->c[flag].points;
+		fprintf(fp, "Contestant <%d>s score decreased by <%d> points to <%d>\n", id+1, points, list->c[flag].points);
 		heapify(list, 0);
         }
 	flag = -1;
 }
 
 void
-showContestants(heap *list, int i){
+showContestants(heap *list, int i, FILE* fp){
+	fprintf(fp, "Contestant <%d> in extended heap location <%d> with score <%d>\n", list->c[i].id+1, i+1, list->c[i].points);
 	if(LCHILD(i) < list->size){
-		showContestants(list, LCHILD(i));
+		showContestants(list, LCHILD(i), fp);
 	}
-	printf("Contestant %d in extended heap location %d with score %d\n", list->c[i].id+1, i+1, list->c[i].points);
+	//fprintf(fp, "Contestant <%d> in extended heap location <%d> with score <%d>\n", list->c[i].id+1, i+1, list->c[i].points);
 	if(RCHILD(i) < list->size){
-		showContestants(list, RCHILD(i));
+		showContestants(list, RCHILD(i), fp);
 	}
 }
 
@@ -227,29 +250,32 @@ showHandles(heap* list, int i, handle* arr){
 }
 
 void
-showLocation(int id, heap* list){
+showLocation(int id, heap* list, FILE* fp){
 	//int flag = -1;
 	searchHeap(list, 0, id);
         if(flag == -1){
-                printf("Contestant %d is not in the heap\n", id);
+                fprintf(fp, "Contestant <%d> is not in the heap\n", id+1);
         }
         else{
-                printf("Contestant %d is in location %d\n", id, flag);
+                fprintf(fp, "Contestant <%d> is in location <%d>\n", id+1, flag);
         }
 	flag = -1;
 }
 
 void
-crownWinner(heap* list, int* arr){
+crownWinner(heap* list, int* arr, FILE *fp){
+	sntl = 1;
 	int total = list->size;
 	for(int i = 0; i < total-1; i++){
-		eliminateWeakest(list, arr);
+		eliminateWeakest(list, arr, fp);
 	}
-	printf("Contestant %d wins with score %d!\n", list->c[0].id+1, list->c[0].points);
+	fprintf(fp, "Contestant <%d> wins with score <%d>!\n", list->c[0].id+1, list->c[0].points);
+	sntl = -1;
 }
 
 int main(int argc, char* argv[]){
 	FILE *fp;
+	FILE *fpo;
 	char buff[1000];
 	//char ins1[1000];
 	char sub1[1000];
@@ -260,6 +286,7 @@ int main(int argc, char* argv[]){
 	int arg1;
 	int arg2;
 	fp = fopen(argv[1], "r");
+	fpo = fopen(argv[2], "a");
 	fscanf(fp, "%d", &size);
 	//size = atoi(buff);
 	//printf("%d\n", size);
@@ -280,10 +307,19 @@ int main(int argc, char* argv[]){
                         len = strlen(buff);
                         strncpy(sub1, &buff[1], len-1);
                         arg2 = atoi(sub1);
-			insertContestant(arg1, arg2, list, arr);
+			fprintf(fpo, "insertContestant <%d> <%d>\n", arg1 + 1, arg2);
+			if(arg1 >= size){
+				//fpo = fopen("output.txt", "a");
+				fprintf(fpo, "Contestant <%d> could not be inserted because the extended heap is full", arg1);
+				//fclose(fpo);
+			}
+			else{
+				insertContestant(arg1, arg2, list, arr, fpo);
+			}
 		}
 		else if(strcmp(buff, "showContestants") == 0){
-			showContestants(list, 0);
+			fprintf(fpo, "showContestants\n");
+			showContestants(list, 0, fpo);
                 }
 		else if(strcmp(buff, "findContestant") == 0){
 			fscanf(fp, "%s", buff);
@@ -291,10 +327,12 @@ int main(int argc, char* argv[]){
                         strncpy(sub1, &buff[1], len-1);
                         arg1 = atoi(sub1);
                         arg1 = arg1 - 1;
-			findContestant(arg1, list);
+			fprintf(fpo, "findContestant <%d>\n", arg1 + 1);
+			findContestant(arg1, list, fpo);
                 }
 		else if(strcmp(buff, "eliminateWeakest") == 0){
-			eliminateWeakest(list, arr);
+			fprintf(fpo, "eliminateWeakest\n");
+			eliminateWeakest(list, arr, fpo);
                 }
 		else if(strcmp(buff, "losePoints") == 0){
 			fscanf(fp, "%s", buff);
@@ -306,7 +344,8 @@ int main(int argc, char* argv[]){
                         len = strlen(buff);
                         strncpy(sub1, &buff[1], len-1);
                         arg2 = atoi(sub1);
-			lostPoints(arg1, arg2, list, arr);
+			fprintf(fpo, "losePoints <%d>, <%d>\n", arg1 + 1, arg2);
+			lostPoints(arg1, arg2, list, arr, fpo);
                 }
 		else if(strcmp(buff, "earnPoints") == 0){
 			fscanf(fp, "%s", buff);
@@ -318,29 +357,40 @@ int main(int argc, char* argv[]){
                         len = strlen(buff);
                         strncpy(sub1, &buff[1], len-1);
                         arg2 = atoi(sub1);
-                        earnPoints(arg1, arg2, list, arr);
+			fprintf(fpo, "earnPoints <%d>, <%d>\n", arg1 + 1, arg2);
+                        earnPoints(arg1, arg2, list, arr, fpo);
                 }
 		else if(strcmp(buff, "showHandles") == 0){
+			fprintf(fpo, "showHandles\n");
 			handle handles[size];
         		for(int i = 0; i < size; i++){
                 		handles[i].flag = 0;
         		}
         		showHandles(list, 0, handles);
         		for(int i = 0; i < size; i ++){
-                		if(handles[i].flag) printf("Contestant %d stored in extended heap location %d\n", i+1, handles[i].spot+1);
-                		else printf("There is no contestant, die!\n");
+                		if(handles[i].flag) fprintf(fpo, "Contestant <%d> stored in extended heap location <%d>\n", i+1, handles[i].spot+1);
+                		else fprintf(fpo, "There is no Contestant <%d> in the extended heap: handle[<%d>] = -1\n", i+1, i+1);
         		}
                 }
 		else if(strcmp(buff, "showLocation") == 0){
-			//showLocation(1, list);
+			fscanf(fp, "%s", buff);
+                        len = strlen(buff);
+                        strncpy(sub1, &buff[1], len-1);
+                        arg1 = atoi(sub1);
+                        arg1 = arg1 - 1;
+			fprintf(fpo, "showLocation <%d>\n", arg1 + 1);
+			showLocation(arg1, list, fpo);
                 }
 		else if(strcmp(buff, "crownWinner") == 0){
-			crownWinner(list, arr);
+			fprintf(fpo, "crownWinner\n");
+			crownWinner(list, arr, fpo);
                 }
 		else{
 			printf("Command not recognized\n");
 		}
 	}
+	fclose(fp);
+	fclose(fpo);
 	//arr[0] = 10;
 	//arr[1] = 20;
 	//arr[2] = 5;
